8e3a27013a31ad2794f89942e41b162f3e364b43
web1992 <554933654@qq.com>
2020-02-12 23:52:57 +0800

    add [MultipleDataSource]

--- src/test/java/cn/web1992/mybatiss/AbstractTest.java
+++ src/test/java/cn/web1992/mybatiss/AbstractTest.java
@@ -6,7 +6,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * 这里关闭事务，如果这里开启事务，事务AOP 总是优选 UserService 上的事务，导致数据源切换失败
+ * 这里关闭事务，如果这里开启事务，事务AOP 总是优选开启，
+ * 那么 UserService 上的事务则不会开启了，导致数据源切换了没有作业
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @Rollback(false)
